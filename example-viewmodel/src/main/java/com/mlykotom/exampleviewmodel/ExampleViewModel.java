6f44a2014306cebbf8a350b602bb65373e810bba
Tomáš Mlynarič <mlynarict@gmail.com>
2017-01-08 18:27:51 +0100

    Renames validation -> mlyked

--- example-viewmodel/src/main/java/com/mlykotom/exampleviewmodel/ExampleViewModel.java
+++ example-viewmodel/src/main/java/com/mlykotom/exampleviewmodel/ExampleViewModel.java
@@ -1,5 +1,8 @@
 package com.mlykotom.exampleviewmodel;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 import com.mlykotom.validation.ValidatedEmailField;
 
 import eu.inloop.viewmodel.AbstractViewModel;
@@ -7,4 +10,10 @@ import eu.inloop.viewmodel.AbstractViewModel;
 
 public class ExampleViewModel extends AbstractViewModel<ExampleView> {
 	public final ValidatedEmailField email = new ValidatedEmailField();
+
+
+	@Override
+	public void onCreate(@Nullable Bundle arguments, @Nullable Bundle savedInstanceState) {
+		super.onCreate(arguments, savedInstanceState);
+	}
 }
