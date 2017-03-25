863c9d1a1d729969ec320d363790ad1f03cf351a
Tomáš Mlynarič <mlynarict@gmail.com>
2017-03-25 18:25:19 +0000

    Split auto example and adds form example

--- example-viewmodel/src/main/java/com/mlykotom/exampleviewmodel/ExampleViewModelActivity.java
+++ example-viewmodel/src/main/java/com/mlykotom/exampleviewmodel/ExampleViewModelActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 
 import com.mlykotom.exampleviewmodel.auto.AutoValidationFragment;
+import com.mlykotom.exampleviewmodel.form.FormValidationFragment;
 import com.mlykotom.exampleviewmodel.manual.ManualValidationFragment;
 
 import eu.inloop.viewmodel.base.ViewModelBaseEmptyActivity;
@@ -49,6 +50,8 @@ public class ExampleViewModelActivity extends ViewModelBaseEmptyActivity {
 
 
 	private static class PagesAdapter extends FragmentStatePagerAdapter {
+		private static String[] sTitles = new String[]{"Single", "Form", "Manual"};
+
 
 		public PagesAdapter(FragmentManager fm) {
 			super(fm);
@@ -63,6 +66,9 @@ public class ExampleViewModelActivity extends ViewModelBaseEmptyActivity {
 					return AutoValidationFragment.newInstance();
 
 				case 1:
+					return FormValidationFragment.newInstance();
+
+				case 2:
 					return ManualValidationFragment.newInstance();
 			}
 		}
@@ -70,20 +76,13 @@ public class ExampleViewModelActivity extends ViewModelBaseEmptyActivity {
 
 		@Override
 		public int getCount() {
-			return 2;
+			return sTitles.length;
 		}
 
 
 		@Override
 		public CharSequence getPageTitle(int position) {
-			switch(position) {
-				case 0:
-				default:
-					return "Auto";
-
-				case 1:
-					return "Manual";
-			}
+			return sTitles[position];
 		}
 	}
 
--- example-viewmodel/src/main/java/com/mlykotom/exampleviewmodel/auto/AutoValidationViewModel.java
+++ example-viewmodel/src/main/java/com/mlykotom/exampleviewmodel/auto/AutoValidationViewModel.java
@@ -4,36 +4,26 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.mlykotom.exampleviewmodel.MyValiFieldCaptcha;
-import com.mlykotom.valifi.ValiFiForm;
-import com.mlykotom.valifi.fields.ValiFieldEmail;
-import com.mlykotom.valifi.fields.ValiFieldPassword;
-import com.mlykotom.valifi.fields.ValiFieldPhone;
 import com.mlykotom.valifi.fields.ValiFieldUsername;
 
 import eu.inloop.viewmodel.AbstractViewModel;
 
 
 public class AutoValidationViewModel extends AbstractViewModel<AutoValidationView> {
-	public final ValiFieldEmail email = new ValiFieldEmail();
 	public final ValiFieldUsername username = new ValiFieldUsername();
-	public final ValiFieldPassword password = new ValiFieldPassword();
-	public final ValiFieldPassword password2 = new ValiFieldPassword();
-	public final ValiFieldPhone phone = new ValiFieldPhone();
 	public final MyValiFieldCaptcha captcha = new MyValiFieldCaptcha();
-	public final ValiFiForm form = new ValiFiForm(email, username, password, password2, phone);
 
 
 	@Override
 	public void onCreate(@Nullable Bundle arguments, @Nullable Bundle savedInstanceState) {
 		super.onCreate(arguments, savedInstanceState);
-		password2.addVerifyFieldValidator("Passwords must be the same", password);
-		phone.setEmptyAllowed(true);
 	}
 
 
 	@Override
 	public void onDestroy() {
-		form.destroy();
+		username.destroy();
+		captcha.destroy();
 		super.onDestroy();
 	}
 }
