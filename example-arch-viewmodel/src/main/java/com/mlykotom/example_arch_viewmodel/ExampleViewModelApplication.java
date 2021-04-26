f7e666ce7da39f89b79778ebf59edf322d334759
Tomáš Mlynarič <mlynarict@gmail.com>
2021-04-26 09:42:24 +0200

    chore: upgrade build files

--- example-arch-viewmodel/src/main/java/com/mlykotom/example_arch_viewmodel/ExampleViewModelApplication.java
+++ example-arch-viewmodel/src/main/java/com/mlykotom/example_arch_viewmodel/ExampleViewModelApplication.java
@@ -3,19 +3,11 @@ package com.mlykotom.example_arch_viewmodel;
 import android.app.Application;
 
 import com.mlykotom.valifi.ValiFi;
-import com.squareup.leakcanary.LeakCanary;
 
 public class ExampleViewModelApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		if (LeakCanary.isInAnalyzerProcess(this)) {
-			// This process is dedicated to LeakCanary for heap analysis.
-			// You should not init your app in this process.
-			return;
-		}
-		LeakCanary.install(this);
-
 		ValiFi.install(this);
 	}
 }
